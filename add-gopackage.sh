9e01b29b59043dc5722e311ac2c03b86e7349ed3
Cosmos Nicolaou <cosmos.nicolaou@gmail.com>
2020-03-04 11:41:45 -0800

    automate adding packages

---
+++ add-gopackage.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# See https://golang.org/cmd/go/#hdr-Legacy_GOPATH_
+
+redirects="content/_redirects"
+
+for pkg in "$@"; do
+	path=$pkg
+	meta="$(basename $path).meta"
+	(
+	echo "${path}	go-get=1	/golang/${meta}	302"
+	echo "${path}/*	go-get=1	/golang/${meta}	302"
+	) >> "${redirects}"
+
+	cat > "content/golang/${meta}" <<!
+<html><head>
+<meta name="go-import" content="cloudeng.io/$pkg git https://github.com/cloudengio/$pkg"/>
+</head></html>
+!
+	git add "content/golang/${meta}"
+done
+sort -su "${redirects}" > "${redirects}.new"
+mv "${redirects}.new" "${redirects}"
+
