efd65717738d1e7f05392f8972a6ae10966c4382
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2023-02-06 10:18:41 -0800

    add cloudeng.io/net

--- add-to-go.pkgs.sh
+++ add-to-go.pkgs.sh
@@ -2,4 +2,4 @@
 
 # See https://golang.org/cmd/go/#hdr-Legacy_GOPATH_
 
-bash add-redirects-etc.sh "go.pkgs" "" algo aws cmdutil debug errors file io os path sync sys text webapp
+bash add-redirects-etc.sh "go.pkgs" "" algo aws cmdutil debug errors file io os path sync sys text webapp net
