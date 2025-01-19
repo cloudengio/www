0e0389315d6cacddaac52701d9e8ed0d8ef6a3aa
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2025-01-19 10:54:02 -0800

    .

--- multi-module-repos.sh
+++ multi-module-repos.sh
@@ -3,6 +3,7 @@
 # See https://golang.org/cmd/go/#hdr-Legacy_GOPATH_
 
 bash add-redirects-etc.sh "citools" "/citools" "citools"
-bash add-redirects-etc.sh "webapi" "/webapi" "webapi"
+bash add-redirects-etc.sh "go.webapi" "/webapi" "webapi"
+bash add-redirects-etc.sh "go.webapps" "/webapps" "webapps"
 bash add-redirects-etc.sh "glean" "/glean" "glean"
 bash add-redirects-etc.sh "go.gotools" "/go" "go"
