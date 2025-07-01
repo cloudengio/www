a51d7c63a966cd7bbe85f0d2ac30032ff5493a5b
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2025-07-01 15:17:55 -0700

    add cloudeng.io/{google,security}

--- add-to-go.pkgs.sh
+++ add-to-go.pkgs.sh
@@ -2,4 +2,4 @@
 
 # See https://golang.org/cmd/go/#hdr-Legacy_GOPATH_
 
-bash add-redirects-etc.sh "go.pkgs" "" algo aws cmdutil debug errors file io os path sync sys text webapp net macos datetime geospatial logging windows linux
+bash add-redirects-etc.sh "go.pkgs" "" algo aws cmdutil debug errors file io os path sync sys text webapp net macos datetime geospatial logging windows linux google security
